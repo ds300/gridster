3f7367d18b765b21e0b62652942cae00ef7c1765
David Sheldrick <david@futurice.com>
2017-10-21 21:34:42 +0100

    lay out page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
+import styled from 'styled-components'
 
 import ConnectedGrid from './ConnectedGrid'
 
@@ -7,15 +8,26 @@ import { makeStore } from './store'
 
 const logo = require('./Gridster-Logo.png')
 
+const Page = styled.div`
+  width: 480px;
+  margin: 0 auto;
+`
+
+const Header = styled.div`
+  margin: 50px 0 35px 0;
+`
+
 class App extends React.Component {
   store = makeStore()
   render() {
     return (
       <Provider store={this.store}>
-        <div>
-          <img src={logo} alt="Gridster" />
+        <Page>
+          <Header>
+            <img src={logo} alt="Gridster" width="225px" />
+          </Header>
           <ConnectedGrid />
-        </div>
+        </Page>
       </Provider>
     )
   }
